9de0a6d578541504863465d0d8335bf49fd4a9a4
azpantiс <petrpetrovich9828@gmail.com>
2022-12-24 19:42:57 +1000

    Some shit

---
+++ Assets/Scriptebls/StatsInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatsInfo", menuName = "ScriptebleObject/StatsInfo", order = 1)]
+public class StatsInfo : ScriptableObject
+{
+    public int healthPoint;
+
+}
