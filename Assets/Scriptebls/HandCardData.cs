6678213bd811691c05aada1d1bb93d553211393b
Александр Ильченко <ilchenko.as@students.dvfu.ru>
2022-12-25 14:37:05 +1000

    Cards ++

--- Assets/Scriptebls/HandCardData.cs
+++ Assets/Scriptebls/HandCardData.cs
@@ -47,8 +47,8 @@ public class HandCardData : ScriptableObject
             targetCard.TakeDamage(effectPower * (int)effectType);
         }
         
-        // Add Mana 
-        // Add Cards
+        ManaManager.GiveMana(manaGain);
+        
     }
 
     private void HandleSpecialUsers()
