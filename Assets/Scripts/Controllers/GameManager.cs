6ad12e856d5a7890799c0d9032f0e7e69617183a
Viktor Alekseev <viktoralekseev1999@gmail.com>
2023-01-10 10:29:37 +1000

    some fix

--- Assets/Scripts/Controllers/GameManager.cs
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -8,27 +8,43 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] Battle battleInfo;
     [SerializeField] BattleSystem.BattleSystem battleSystem;
+    [SerializeField] TableConroller tableConroller;
 
 
+    [SerializeField] CardOnTable cardOnTablePrefab;
 
-    private void Awake()
+
+    [SerializeField] List<CreatureData> creatureData;
+
+    void Start()
     {
         battleSystem.LoadBattle(battleInfo);
 
-        foreach(var data in battleSystem.Context.Field.Where(i => i != null)) {
-            SpawnCreture(data.creatureData);
+
+
+         
+        foreach (var creatureData in battleSystem.Context.Field[0..4].Where(i => i != null).Select(i => i.creatureData))  
+        {
+            var newCard = Instantiate(cardOnTablePrefab);
+            newCard.LoadFromCreatureData(creatureData);
+
+            tableConroller.playerCards.Add(newCard);
+
+            tableConroller.palyerCardsDict[creatureData] = newCard;
         };
+
+        foreach (var creatureData in battleSystem.Context.Field[5..9].Where(i => i != null).Select(i => i.creatureData))  
+        {
+            var newCard = Instantiate(cardOnTablePrefab);
+            newCard.LoadFromCreatureData(creatureData);
+
+            tableConroller.enemyCards.Add(newCard);
+
+            tableConroller.enemyCardsDict[creatureData] = newCard;
+        };
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void SpawnCreture(CreatureData creatureData)
-    {
-
-    }
 
 }
--- Assets/Scripts/Table/CardOnTable.cs
+++ Assets/Scripts/Table/CardOnTable.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CardOnTable : Card
 {
-    
+
+    [SerializeField] private TextMeshPro cardName;
+    [SerializeField] private TextMeshPro cardHealth;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    public void LoadFromCreatureData(CreatureData creatureData)
+    {
+        Debug.Log(cardName);
+        Debug.Log(creatureData);
+        cardName.text = creatureData.name;
+        cardHealth.text = creatureData.Health.ToString();
+        spriteRenderer.sprite = creatureData.Sprite;
+        
+    }
+
+
 }
