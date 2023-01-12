e5653c385f789fa6b701d3cd7981b44607ff45c8
Viktor Alekseev <viktoralekseev1999@gmail.com>
2023-01-13 03:52:01 +1000

    вроде должно работать

--- Assets/Scripts/Controllers/ManaController.cs
+++ Assets/Scripts/Controllers/ManaController.cs
@@ -7,17 +7,22 @@ using BattleSystem;
 public class ManaController : MonoBehaviour
 {
 
-    public static int CurrentManaValue = 5;
+    public static int CurrentManaValue = 3;
+    public static int MaxManatManaValue = 5;
 
     [SerializeField] private TextMeshPro Mana;
 
     private ManaController instance;
 
-    private void Awake()
+    private void Start()
     {
         instance = (instance == null) ? this : instance;
 
-        Context.ManaChanged += (int amount) => Mana.text = amount.ToString(); 
+        Context.ManaChanged += (int amount) => 
+        { 
+            CurrentManaValue= amount;
+            Mana.text = $"{amount}/{MaxManatManaValue}"; };
+
     }
 
 
